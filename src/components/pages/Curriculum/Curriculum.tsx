92efba90f08864770f691f4160bb1b3579bec059
Pedro Cazala <cazalapedro@gmail.com>
2023-03-27 19:10:52 -0300

    ahora se puede descargar cv como pdf

--- src/components/pages/Curriculum/Curriculum.tsx
+++ src/components/pages/Curriculum/Curriculum.tsx
@@ -11,9 +11,12 @@ function Curriculum() {
                     <i className="bi bi-file-pdf"></i> Ver
                 </button>
             </Link>
-            <button type="button" className="btn btn-primary">
-                <i className="bi bi-download"></i> Descargar
-            </button>
+            <a href="/cv.pdf" download="cv-Pedro_Cazalá.pdf">
+                <button type="button" className="btn btn-primary">
+                    <i className="bi bi-download"></i> Descargar
+                </button>
+            </a>
+
             <ViewCV />
         </div>
     );
--- src/components/pages/Curriculum/ViewCV.tsx
+++ src/components/pages/Curriculum/ViewCV.tsx
@@ -1,7 +1,7 @@
 import { Document, Page } from 'react-pdf';
 
 export default function ViewCV() {
-    const url = './cv.pdf'
+    const url = __dirname + '/cv.pdf'
   return (
     <Document file={url}>
       <Page pageNumber={1} />
